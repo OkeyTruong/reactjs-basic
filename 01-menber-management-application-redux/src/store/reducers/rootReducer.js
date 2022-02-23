4f7080c82e12406a9cdcf3e3f14f0d49025ac050
huy <okey222k@gmail.com>
2022-02-23 19:47:07 +0700

    redux setup

---
+++ 01-menber-management-application-redux/src/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const initialState = {
+    members:[
+        {id:1, name:"Huy"},
+        {id:2, name:"Okey"},
+        {id:3, name:"lh222k"},
+    ]
+}
+const rootReducer = (state = initialState , action) => {
+    return state
+}
+
+export default rootReducer
